9e356f9e221776fb8a3b8b56094b129c23e7c156
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-05-24 23:38:30 -0400

    added js files, initiated inquirer

---
+++ index.js
@@ -0,0 +1,21 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// 1. User input
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
+
+
+// 2. Create .prompt using inquirer
+// 3. Add objects with properties for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// 4. When input is entered console response in README file *add (err)
