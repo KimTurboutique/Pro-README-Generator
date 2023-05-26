827d07bef402f6f95ecafa573d9af151a540da6c
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-05-26 08:40:43 -0400

    add inquirer prompts

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // 1. User input
+// 2. Create .prompt using inquirer
+// 3. Add objects with properties for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 // TODO: Create an array of questions for user input
 const questions = [];
 inquirer
     .prompt([
         {
             type: 'input',
-            name: 
-        }
-    ])
+            name: 'title',
+            message: 'Enter project title',
+        },
+        {
+            type: 'input'
+        },
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
@@ -23,6 +29,5 @@ function init() { }
 init();
 
 
-// 2. Create .prompt using inquirer
-// 3. Add objects with properties for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// 4. When input is entered console response in README file *add (err)
+
+// 4. When input is entered show data in README file *add (err)
