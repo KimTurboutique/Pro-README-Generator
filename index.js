4453aaa47b7b0981720cae58cd0e0a61a01851a9
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-05-31 18:56:20 -0400

    added licenses,table of contents, different sections

--- index.js
+++ index.js
@@ -1,29 +1,72 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // 1. User input
 // 2. Create .prompt using inquirer
 // 3. Add objects with properties for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 // TODO: Create an array of questions for user input
-const questions = [];
-inquirer
-    .prompt([
+const questions = [
         {
             type: 'input',
             name: 'title',
             message: 'Enter project title',
         },
         {
-            type: 'input'
+            type: 'input',
+            name: 'description',
+            message: 'Write a short description of your project',
         },
-    ]);
+        {
+            type:'input',
+            name: 'installation',
+            message: 'How does someone install this application?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How to use this application?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license is this project using?',
+            choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "none"],
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'How does someone contribute to this project?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be run to test?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is GitHub username?',
+        },
+    ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions)
+    .then(response=>{
+        fs.writeFileSync("READ.md",generateMarkdown(response))
+    console.log("Your ReadMe was successfully created")
+})
+ }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return ` ![Github license](https://img.shields.io/badge/license-${license.replace(/ /g, '%20')}-blue.svg)`;
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `- [License](#license)`
+  }
+  return ""
+ }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License\nthis project is licensed under the ${license} license`
+  }
+  return ""
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table Of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions email at ${data.email}\n
+  If you want to see more of my work you can visit my GitHub page [${data.github}](https://github.com/${data.github})
+  
+
 
 `;
 }
